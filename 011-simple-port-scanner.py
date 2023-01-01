91d8ba358e4fd21ac1e0fd79298af99c56eafc48
Jann Moon <35894688+scumdestroy@users.noreply.github.com>
2023-01-01 06:07:10 -0800

    Update 011-simple-port-scanner.py
    changed port_range to a custom list of interesting ports instead

--- 011-simple-port-scanner.py
+++ 011-simple-port-scanner.py
@@ -4,11 +4,14 @@ import struct
 # The IP address range to scan
 ip_dest = argv[1]
 
-# The port range to scan
-PORT_RANGE = range(1, 1024)
+# Commonly used ports + a few of my favorite vulnerable bug bounty/interesting shodan-able ones thrown in
+common_ports = [80, 137, 433, 123, 138, 1434, 445, 136, 67, 23, 21, 139, 22, 68, 8080, 8433, 8080, 25, 111, 110, 998, 53, 2222, 135, 3306, 2049, 1433, 3456, 443, 7, 5900]
 
+# Alternate: use range of ports
+# PORT_RANGE = range(1, 1024)
 # Iterate through each port in the range
-for port in PORT_RANGE:
+# for port in PORT_RANGE:
+for port in common_ports:
     # Set the destination port
     dest_addr = (ip_dest, port)
 
@@ -28,7 +31,7 @@ for port in PORT_RANGE:
                 break
 
         # Print the results
-        print(f"{addr[0]}:{addr[1]} is open (TTL: {ttl}, guessed OS: {os_guess})")
+        print(f"{addr[0]}:{addr[1]} is open (TTL: {ttl}, likely OS: {os_guess})")
     except socket.timeout:
         # If no response is received, the port is closed
         pass
